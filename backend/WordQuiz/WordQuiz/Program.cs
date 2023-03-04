624f034e258d368c6ef877a5fb560e83e8a977f9
torokt21 <torokt21@gmail.com>
2023-03-04 11:40:28 +0100

    Update identity user to player

--- backend/WordQuiz/WordQuiz/Program.cs
+++ backend/WordQuiz/WordQuiz/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WordQuiz.Data;
+using WordQuiz.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+builder.Services.AddDefaultIdentity<Player>(options => {
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireDigit = false;
     options.Password.RequiredLength = 6;
