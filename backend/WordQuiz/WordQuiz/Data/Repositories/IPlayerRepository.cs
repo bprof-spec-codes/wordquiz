77dcfbd62b7cbfd2e16cc0f2e20a276ef46627cb
Dávid Kusztos <kusztosdavid@gmail.com>
2023-03-04 19:37:58 +0100

    PlayerRepo with interface done.

---
+++ backend/WordQuiz/WordQuiz/Data/Repositories/IPlayerRepository.cs
@@ -0,0 +1,13 @@
+﻿using WordQuiz.Models;
+
+namespace WordQuiz.Data.Repositories
+{
+    public interface IPlayerRepository
+    {
+        Task<Player> CreatePlayer(Player player);
+        Task DeletePlayer(string playerId);
+        Task<IEnumerable<Player>> GetAllPlayers();
+        Task<Player> GetPlayerById(string playerId);
+        Task<Player> UpdatePlayer(Player player);
+    }
+}
---
+++ backend/WordQuiz/WordQuiz/Data/Repositories/PlayerRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WordQuiz.Models;
+
+namespace WordQuiz.Data.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PlayerRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        public async Task<IEnumerable<Player>> GetAllPlayers()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        public async Task<Player> GetPlayerById(string playerId)
+        {
+            return await _dbContext.Users.FindAsync(playerId);
+        }
+
+        public async Task<Player> CreatePlayer(Player player)
+        {
+            _dbContext.Users.Add(player);
+            await _dbContext.SaveChangesAsync();
+            return player;
+        }
+
+        public async Task<Player> UpdatePlayer(Player player)
+        {
+            _dbContext.Entry(player).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return player;
+        }
+
+        public async Task DeletePlayer(string playerId)
+        {
+            var player = await _dbContext.Users.FindAsync(playerId);
+            if (player != null)
+            {
+                _dbContext.Users.Remove(player);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+
+}
