596d7abf91b1b4cb4b9c5a2cb5bb4e090aafaa55
Dávid Kusztos <kusztosdavid@gmail.com>
2023-03-04 19:44:38 +0100

    WordStatisticRepo with interface done.

---
+++ backend/WordQuiz/WordQuiz/Data/Repositories/IWordStaticRepository.cs
@@ -0,0 +1,13 @@
+﻿using WordQuiz.Models;
+
+namespace WordQuiz.Data.Repositories
+{
+    public interface IWordStaticRepository
+    {
+        Task AddAsync(WordStatistic entity);
+        Task DeleteAsync(string id);
+        Task<IEnumerable<WordStatistic>> GetAllAsync();
+        Task<WordStatistic> GetByIdAsync(string id);
+        Task UpdateAsync(WordStatistic entity);
+    }
+}
---
+++ backend/WordQuiz/WordQuiz/Data/Repositories/WordStaticRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WordQuiz.Models;
+
+namespace WordQuiz.Data.Repositories
+{
+    public class WordStaticRepository : IWordStaticRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WordStaticRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<WordStatistic>> GetAllAsync()
+        {
+            return await _context.WordStatistics.Include(ws => ws.Player).Include(ws => ws.Word).ToListAsync();
+        }
+
+        public async Task<WordStatistic> GetByIdAsync(string id)
+        {
+            return await _context.WordStatistics.Include(ws => ws.Player).Include(ws => ws.Word).FirstOrDefaultAsync(ws => ws.Id == id);
+        }
+
+        public async Task AddAsync(WordStatistic entity)
+        {
+            await _context.WordStatistics.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(WordStatistic entity)
+        {
+            _context.WordStatistics.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var wordStatistic = await _context.WordStatistics.FindAsync(id);
+            _context.WordStatistics.Remove(wordStatistic);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+}
