36286a170b9edf4a894faf4a2053118b2a28fb40
torokt21 <torokt21@gmail.com>
2023-03-11 20:37:23 +0100

    Add topic selection to routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 import { SecondaryRegionComponent } from './secondary-region/secondary-region.component';
-import { FooterComponent } from './footer/footer.component';
 import { TopicSelectionComponent } from './topic-selection/topic-selection.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, SecondaryRegionComponent, FooterComponent, TopicSelectionComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SecondaryRegionComponent,
+    FooterComponent,
+    TopicSelectionComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'topics', component: TopicSelectionComponent },
+    ]),
     NgbModule,
   ],
   providers: [],
