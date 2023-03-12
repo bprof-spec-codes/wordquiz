ffab756ff1c89a8e13f2637100598ad1062dcbbc
torokt21 <torokt21@gmail.com>
2023-03-12 19:04:56 +0100

    Add topic selection

--- frontend/src/app/topic-selection/topic-selection.component.ts
+++ frontend/src/app/topic-selection/topic-selection.component.ts
@@ -1,10 +1,21 @@
+import { Topic, TopicService } from '../topic.service';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-topic-selection',
   templateUrl: './topic-selection.component.html',
-  styleUrls: ['./topic-selection.component.scss']
+  styleUrls: ['./topic-selection.component.scss'],
 })
 export class TopicSelectionComponent {
+  topics!: Topic[];
 
+  /**
+   *
+   */
+  constructor(private topicService: TopicService) {}
+
+  ngOnInit() {
+    this.topics = this.topicService.getAll();
+  }
 }
