eac37a591414ac30fe704b6604e843449562d933
Kuszter <92097700+Kuszter@users.noreply.github.com>
2023-05-27 19:10:12 +0200

    Debugging Gamelogic

---
+++ backend/WordQuiz/WordQuiz/Models/PlayerStatistics.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using WordQuiz.Models;
+
+namespace WordQuiz.Models
+{
+    public class PlayerStatistics
+    {
+        public string PlayerId { get; set; }
+        public List<WordStatistic> WordStatistics { get; set; }
+
+        public PlayerStatistics()
+        {
+            WordStatistics = new List<WordStatistic>();
+        }
+    }
+}
