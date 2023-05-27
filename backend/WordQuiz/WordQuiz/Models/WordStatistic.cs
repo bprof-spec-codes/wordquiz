b4f53b53cedbb1859bbc8864cd81345af10e3523
Kuszter <92097700+Kuszter@users.noreply.github.com>
2023-05-27 19:15:32 +0200

    Guessinput added

--- backend/WordQuiz/WordQuiz/Models/WordStatistic.cs
+++ backend/WordQuiz/WordQuiz/Models/WordStatistic.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WordQuiz.Models
@@ -10,15 +11,16 @@ namespace WordQuiz.Models
         public string Id { get; set; }
         public string PlayerId { get; set; }
         public string WordId { get; set; }
+        public int CorrectGuesses { get; set; }  // Added this
+        public int IncorrectGuesses { get; set; }  // And this
         public int Score { get; set; }
 
-        
         [NotMapped]
         public Player Player { get; set; }
 
         [NotMapped]
         public Word Word { get; set; }
-        
+
         public WordStatistic()
         {
             this.Id = Guid.NewGuid().ToString();
